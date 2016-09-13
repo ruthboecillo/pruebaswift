05661e24de93070922031cfc7e34d58d29e37695
Roberto Samaniego Millán <r.boecillo@gmail.com>
2016-09-13 20:46:54 +0200

    reto2

---
+++ funciones/Reto1.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+//: Playground - noun: a place where people can play
+/* programa  coursera modulo 2. MINI RETO
+ */
+
+
+var salida = ""
+
+
+for numero in 0...100
+{
+    salida = "#\(numero) "
+    
+    
+    if (numero % 5) == 0
+    {
+        salida += "Bingo!!! "
+    }
+    if (numero % 2) == 0
+    {
+        salida += "par!!! "
+    }
+    else
+    {
+        salida += "impar!!! "
+    }
+    if (numero > 19 && numero < 41)
+    {
+        salida += "viva Swift!!"
+    }
+    
+    print ("\(salida)" )
+}
+
